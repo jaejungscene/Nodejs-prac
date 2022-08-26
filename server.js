693454bfade603174d000818ebc709b786faf5d9
Jaejung <ljj0512@gmail.com>
2022-08-26 12:01:29 +0900

    MODIFY get API in server.js

--- server.js
+++ server.js
@@ -7,35 +7,30 @@ const port = 8080;
 app.use(express.json());
 app.use(cors());
 
+// get all products
 app.get("/products", (req, res)=>{
-    const query = req.query;
-    console.log("query : ", query);
-    res.send({
-        "products" : [
-            {
-                "id" : 1,
-                "name": "농구공",
-                "price": 100000,
-                "seller": "조던",
-                "imageUrl": "images/products/basketball1.jpeg"
-            },
-            {
-                "id": 2,
-                "name": "축구공",
-                "price": 50000,
-                "seller": "메시",
-                "imageUrl": "images/products/soccerball1.jpg"
-            },
-            {
-                "id" : 3,
-                "name": "키보드",
-                "price": 10000,
-                "seller": "그랩",
-                "imageUrl": "images/products/keyboard1.jpg"
-            }
-      ]
+    models.Product.findAll({ // find data in database
+        order:[
+            ["id","ASC"],
+        ],
+        attributes:[
+            "id",
+            "name",
+            "price",
+            "createdAt",
+            "seller",
+            "imageUrl"
+        ]
+    }).then((result)=>{ // if result exits, send data
+        console.log("PRODUCTS: ", result);
+        res.send({
+            products : result
+        })
+    }).catch((error)=>{
+        console.error(error);
+        res.send("error occur");
     });
-})
+});
 
 app.post("/products", (req,res)=>{
     const body = req.body;
@@ -43,7 +38,7 @@ app.post("/products", (req,res)=>{
     if(!name || !description || !price || !seller){
         res.send("모든 필드를 입력해주세요.");
     }
-    models.Products.create({
+    models.Product.create({
         name,
         description,
         price,
@@ -56,14 +51,26 @@ app.post("/products", (req,res)=>{
     }).catch((error)=>{
         console.error(error);
         res.send("상품 업로드에 문제가 발생했습니다.");
-    });
-})
+    })
+});
 
 app.get("/products/:id", (req,res)=>{
     const params = req.params;
     const {id} = params;
-    res.send(`id는 ${id}입니다.`);
-})
+    models.Product.findOne({
+        where:{
+            id: id
+        }
+    }).then((result)=>{
+        console.log("PRODUCT:" , result);
+        res.send({
+            product: result,
+        });
+    }).catch((error)=>{
+        console.eeror(error);
+        res.send("상품 조회에 에러가 발생했습니다.");
+    });
+});
 
 app.listen(port, ()=>{
     console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다");
